588b552ad68d416f87de97182c6114ddebfabf94
unknown <suryanshpatwal@gmail.com>
2024-09-24 07:25:42 +0530

    push krna tha bas

--- src/Components/SideNav.jsx
+++ src/Components/SideNav.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const SideNav = () => {
   return (
-    <box>SideNav</box>
+    <box>editing hogi yaha bhot kaidi
+        <br />
+        bhen ke land autat mai samjha
+        <br /> 
+        ochi hai apun ochi
+        <br />
+        dhee chod ke 
+    </box>
   )
 }
 
